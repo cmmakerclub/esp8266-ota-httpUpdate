9d0be98229cf6d18ff8584017211c1dcf7d209a9
Nat <nat@cmmc.io>
2017-03-06 23:48:18 +0700

    update

--- esp8266-ota-httpUpdate/esp8266-ota-httpUpdate.ino
+++ esp8266-ota-httpUpdate/esp8266-ota-httpUpdate.ino
@@ -49,7 +49,6 @@ void loop() {
       case HTTP_UPDATE_NO_UPDATES:
         Serial.println("HTTP_UPDATE_NO_UPDATES");
         break;
-
       case HTTP_UPDATE_OK:
         Serial.println("HTTP_UPDATE_OK");
         break;
